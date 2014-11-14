a30f097f75d59bb7ce9563923a6d6896bc7aa0ea
mksy <maksik.boy@gmail.com>
2014-11-15 00:24:55 +0200

    Fix
    Change-Id: I074943b9e8c4cd9bf2d70b1da36c7e99c32dce7f

--- shadow-common/shadow-vendor.mk
+++ shadow-common/shadow-vendor.mk
@@ -16,7 +16,6 @@ vendor_path = vendor/motorola/shadow-common
 
 # LIBS
 PRODUCT_COPY_FILES += \
-	$(vendor_path)/lib/libaudio.so:obj/lib/libaudio.so \
 	$(vendor_path)/lib/libcamera.so:obj/lib/libcamera.so \
 	$(vendor_path)/lib/libril_rds.so:obj/lib/libril_rds.so \
 	$(vendor_path)/lib/libbattd.so:system/lib/libbattd.so \
@@ -34,6 +33,9 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/libmotodbgutils.so:system/lib/libmotodbgutils.so \
 	$(vendor_path)/lib/libPERF.so:system/lib/libPERF.so \
 	$(vendor_path)/lib/libOMX.TI.720P.Decoder.so:system/lib/libOMX.TI.720P.Decoder.so \
+	$(vendor_path)/lib/libOMX.TI.720P.Encoder.so:system/lib/libOMX.TI.720P.Encoder.so \
+	$(vendor_path)/lib/libOMX.TI.mp4.splt.Encoder.so:system/lib/libOMX.TI.mp4.splt.Encoder.so \
+	$(vendor_path)/lib/libOMX.TI.h264.splt.Encoder.so:system/lib/libOMX.TI.h264.splt.Encoder.so \
 	$(vendor_path)/lib/libOMX.TI.JPEG.Encoder.so:system/lib/libOMX.TI.JPEG.Encoder.so \
 	$(vendor_path)/lib/libnativedrm1.so:system/lib/libnativedrm1.so \
 	$(vendor_path)/lib/libpkip.so:system/lib/libpkip.so \
@@ -41,8 +43,6 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/libprovlib.so:system/lib/libprovlib.so \
 	$(vendor_path)/lib/libscalado.so:system/lib/libscalado.so \
 	$(vendor_path)/lib/libssmgr.so:system/lib/libssmgr.so \
-	$(vendor_path)/lib/libaudio.so:system/lib/libaudio.so \
-	$(vendor_path)/lib/libaudiopolicy.so:system/lib/libaudiopolicy.so \
 	$(vendor_path)/lib/libextdisp.so:system/lib/libextdisp.so \
 	$(vendor_path)/lib/libhdmi.so:system/lib/libhdmi.so \
 	$(vendor_path)/lib/libcamera.so:system/lib/libcamera.so \
@@ -63,27 +63,45 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/libtpa.so:system/lib/libtpa.so \
 	$(vendor_path)/lib/libtpa_core.so:system/lib/libtpa_core.so \
 	$(vendor_path)/lib/hw/gps.shadow.so:system/lib/hw/gps.shadow.so \
-	$(vendor_path)/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
-	$(vendor_path)/lib/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
-	$(vendor_path)/lib/dsp/h264vdec_sn.dll64P:system/lib/dsp/h264vdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/h264venc_sn.dll64P:system/lib/dsp/h264venc_sn.dll64P \
-	$(vendor_path)/lib/dsp/jpegdec_sn.dll64P:system/lib/dsp/jpegdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/jpegenc_sn.dll64P:system/lib/dsp/jpegenc_sn.dll64P \
-	$(vendor_path)/lib/dsp/m4venc_sn.dll64P:system/lib/dsp/m4venc_sn.dll64P \
-	$(vendor_path)/lib/dsp/mp3dec_sn.dll64P:system/lib/dsp/mp3dec_sn.dll64P \
-	$(vendor_path)/lib/dsp/mp4vdec_sn.dll64P:system/lib/dsp/mp4vdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/mpeg4aacdec_sn.dll64P:system/lib/dsp/mpeg4aacdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/mpeg4aacenc_sn.dll64P:system/lib/dsp/mpeg4aacenc_sn.dll64P \
+
+	# dsp
+	PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/dsp/nbamrdec_sn.dll64P:system/lib/dsp/nbamrdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/nbamrenc_sn.dll64P:system/lib/dsp/nbamrenc_sn.dll64P \
-	$(vendor_path)/lib/dsp/ringio.dll64P:system/lib/dsp/ringio.dll64P \
-	$(vendor_path)/lib/dsp/usn.dll64P:system/lib/dsp/usn.dll64P \
-	$(vendor_path)/lib/dsp/wbamrdec_sn.dll64P:system/lib/dsp/wbamrdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/wbamrenc_sn.dll64P:system/lib/dsp/wbamrenc_sn.dll64P \
-	$(vendor_path)/lib/dsp/720p_h264vdec_sn.dll64P:system/lib/dsp/720p_h264vdec_sn.dll64P \
-	$(vendor_path)/lib/dsp/720p_mp4vdec_sn.dll64P:system/lib/dsp/720p_mp4vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/monitor_tiomap3430.dof64P:system/lib/dsp/monitor_tiomap3430.dof64P \
+	$(vendor_path)/lib/dsp/star.l64P:system/lib/dsp/star.l64P \
+	$(vendor_path)/lib/dsp/h264vdec_sn.dll64P:system/lib/dsp/h264vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
+	$(vendor_path)/lib/dsp/m4venc_sn.dll64P:system/lib/dsp/m4venc_sn.dll64P \
+	$(vendor_path)/lib/dsp/g729dec_sn.dll64P:system/lib/dsp/g729dec_sn.dll64P \
+	$(vendor_path)/lib/dsp/qosdyn_3430.dll64P:system/lib/dsp/qosdyn_3430.dll64P \
+	$(vendor_path)/lib/dsp/720p_divx_mp4vdec_sn.dll64P:system/lib/dsp/720p_divx_mp4vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/mpeg4aacdec_sn.dll64P:system/lib/dsp/mpeg4aacdec_sn.dll64P \
 	$(vendor_path)/lib/dsp/720p_h264venc_sn.dll64P:system/lib/dsp/720p_h264venc_sn.dll64P \
+	$(vendor_path)/lib/dsp/dctn_dyn.dll64P:system/lib/dsp/dctn_dyn.dll64P \
+	$(vendor_path)/lib/dsp/dfgm.dll64P:system/lib/dsp/dfgm.dll64P \
+	$(vendor_path)/lib/dsp/ilbcdec_sn.dll64P:system/lib/dsp/ilbcdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/mp3dec_sn.dll64P:system/lib/dsp/mp3dec_sn.dll64P \
+	$(vendor_path)/lib/dsp/wbamrdec_sn.dll64P:system/lib/dsp/wbamrdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/jpegenc_sn.dll64P:system/lib/dsp/jpegenc_sn.dll64P \
+	$(vendor_path)/lib/dsp/720p_h264vdec_sn.dll64P:system/lib/dsp/720p_h264vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/chromasuppress.l64p:system/lib/dsp/chromasuppress.l64p \
+	$(vendor_path)/lib/dsp/ddspbase_tiomap3430.dof64P:system/lib/dsp/ddspbase_tiomap3430.dof64P \
+	$(vendor_path)/lib/dsp/dynbase_tiomap3430.dof64P:system/lib/dsp/dynbase_tiomap3430.dof64P \
+	$(vendor_path)/lib/dsp/ringio.dll64P:system/lib/dsp/ringio.dll64P \
+	$(vendor_path)/lib/dsp/h264venc_sn.dll64P:system/lib/dsp/h264venc_sn.dll64P \
+	$(vendor_path)/lib/dsp/720p_wmv9vdec_sn.dll64P:system/lib/dsp/720p_wmv9vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/g726dec_sn.dll64P:system/lib/dsp/g726dec_sn.dll64P \
+	$(vendor_path)/lib/dsp/nbamrenc_sn.dll64P:system/lib/dsp/nbamrenc_sn.dll64P \
+	$(vendor_path)/lib/dsp/usn.dll64P:system/lib/dsp/usn.dll64P \
+	$(vendor_path)/lib/dsp/720p_mp4vdec_sn.dll64P:system/lib/dsp/720p_mp4vdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/baseimage.map:system/lib/dsp/baseimage.map \
+	$(vendor_path)/lib/dsp/g711dec_sn.dll64P:system/lib/dsp/g711dec_sn.dll64P \
+	$(vendor_path)/lib/dsp/ilbcenc_sn.dll64P:system/lib/dsp/ilbcenc_sn.dll64P \
+	$(vendor_path)/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
+	$(vendor_path)/lib/dsp/mp4vdec_sn.dll64P:system/lib/dsp/mp4vdec_sn.dll64P \
 	$(vendor_path)/lib/dsp/720p_mp4venc_sn.dll64P:system/lib/dsp/720p_mp4venc_sn.dll64P \
+	$(vendor_path)/lib/dsp/jpegdec_sn.dll64P:system/lib/dsp/jpegdec_sn.dll64P \
+	$(vendor_path)/lib/dsp/mpeg4aacenc_sn.dll64P:system/lib/dsp/mpeg4aacenc_sn.dll64P
 
 # Frameworks
 PRODUCT_COPY_FILES += \
@@ -111,21 +129,21 @@ PRODUCT_COPY_FILES += \
 
 # TI SGX DDK 1.8 ES 5.x
 PRODUCT_COPY_FILES += \
-	$(vendor_path)/ti_sgx_es5.x/gralloc.omap3.so:system/lib/hw/gralloc.omap3.so \
-	$(vendor_path)/ti_sgx_es5.x/libEGL_POWERVR_SGX530_125.so:system/lib/egl/libEGL_POWERVR_SGX530_125.so \
-	$(vendor_path)/ti_sgx_es5.x/libGLESv1_CM_POWERVR_SGX530_125.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX530_125.so \
-	$(vendor_path)/ti_sgx_es5.x/libGLESv2_POWERVR_SGX530_125.so:system/lib/egl/libGLESv2_POWERVR_SGX530_125.so \
-	$(vendor_path)/ti_sgx_es5.x/libpvr2d.so:system/lib/libpvr2d.so \
-	$(vendor_path)/ti_sgx_es5.x/libeglinfo.so:system/lib/libeglinfo.so \
-	$(vendor_path)/ti_sgx_es5.x/libusc.so:system/lib/libusc.so \
-	$(vendor_path)/ti_sgx_es5.x/libglslcompiler.so:system/lib/libglslcompiler.so \
-	$(vendor_path)/ti_sgx_es5.x/libpvrANDROID_WSEGL.so:system/lib/libpvrANDROID_WSEGL.so \
-	$(vendor_path)/ti_sgx_es5.x/libsrv_um.so:system/lib/libsrv_um.so \
-	$(vendor_path)/ti_sgx_es5.x/libIMGegl.so:system/lib/libIMGegl.so \
-	$(vendor_path)/ti_sgx_es5.x/libsrv_init.so:system/lib/libsrv_init.so \
-	$(vendor_path)/ti_sgx_es5.x/libPVRScopeServices.so:system/lib/libPVRScopeServices.so \
-	$(vendor_path)/ti_sgx_es5.x/pvrsrvctl:system/bin/pvrsrvctl \
-	$(vendor_path)/ti_sgx_es5.x/pvrsrvinit:system/bin/pvrsrvinit \
+	$(vendor_path)/ti_sgx_1.8/gralloc.omap3.so:system/vendor/lib/hw/gralloc.omap3.so \
+	$(vendor_path)/ti_sgx_1.8/libEGL_POWERVR_SGX530_125.so:system/vendor/lib/egl/libEGL_POWERVR_SGX530_125.so \
+	$(vendor_path)/ti_sgx_1.8/libGLESv1_CM_POWERVR_SGX530_125.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX530_125.so \
+	$(vendor_path)/ti_sgx_1.8/libGLESv2_POWERVR_SGX530_125.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX530_125.so \
+	$(vendor_path)/ti_sgx_1.8/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+	$(vendor_path)/ti_sgx_1.8/libeglinfo.so:system/vendor/lib/libeglinfo.so \
+	$(vendor_path)/ti_sgx_1.8/libusc.so:system/vendor/lib/libusc.so \
+	$(vendor_path)/ti_sgx_1.8/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+	$(vendor_path)/ti_sgx_1.8/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+	$(vendor_path)/ti_sgx_1.8/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+	$(vendor_path)/ti_sgx_1.8/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+	$(vendor_path)/ti_sgx_1.8/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+	$(vendor_path)/ti_sgx_1.8/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+	$(vendor_path)/ti_sgx_1.8/pvrsrvctl:system/bin/pvrsrvctl \
+	$(vendor_path)/ti_sgx_1.8/pvrsrvinit:system/bin/pvrsrvinit \
 
 # bin
 PRODUCT_COPY_FILES += \
